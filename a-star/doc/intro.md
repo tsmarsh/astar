c0683e5d66ba2e77504c3f7a42f9169dfc686910
Tom Marsh <ts.marsh@gmail.com>
2013-09-08 14:04:45 -0400

    A Star implemented in Clojure

---
+++ a-star/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to a-star
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ a-star/test/a_star/core_test.clj
@@ -0,0 +1,7 @@
+(ns a-star.core-test
+  (:use clojure.test
+        a-star.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
